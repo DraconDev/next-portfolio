a603a2cecf854aab0e1112f3a76e7bd6708f91a4
DraconDev <dracsharp@gmail.com>
2023-12-10 19:49:56 +0000

    upd workcard

--- components/Work/WorkCard.tsx
+++ components/Work/WorkCard.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { FaGithub } from "react-icons/fa";
 
 import { Project } from "@/state/Context";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
     project: Project;
@@ -10,15 +12,24 @@ type Props = {
 const WorkCard = (props: Props) => {
     return (
         <div className="w-full flex flex-col">
-            <h1 className="text-3xl">{props.project.name}</h1>
-            <Image
-                src={`${props.project.image_path}`}
-                alt={`${props.project.name}`}
-                width={1200}
-                height={800}
-                className="object-cover border-2 border-secondary"
-            />
-
+            <div className="flex justify-center gap-3 items-center p-1">
+                <div className="text-xl md:text-3xl">{props.project.name}</div>
+                <Link
+                    href={props.project.github_link}
+                    className=" "
+                >
+                    <FaGithub className="w-8 h-8" />
+                </Link>
+            </div>
+            <Link href={props.project.host_link}>
+                <Image
+                    src={`${props.project.image_path}`}
+                    alt={`${props.project.name}`}
+                    width={1200}
+                    height={800}
+                    className="object-cover border-2 border-secondary"
+                />
+            </Link>
             <div className="sm:text-sm md:text-xl gap-1 flex justify-center p-2 pb-6 flex-wrap">
                 {props.project.description.split(" ").map((item, index) => (
                     <div
