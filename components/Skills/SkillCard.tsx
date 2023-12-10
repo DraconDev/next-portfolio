bd47a5a2792bc30f77739f54ad2bcdd872cc9d02
DraconDev <dracsharp@gmail.com>
2023-12-10 17:48:35 +0000

    add skillcard

--- components/Skills/SkillCard.tsx
+++ components/Skills/SkillCard.tsx
@@ -1,7 +1,19 @@
-type Props = {};
+type Props = {
+    name: string;
+    items: string[];
+};
 
 const SkillCard = (props: Props) => {
-    return <div>SkillCard</div>;
+    return (
+        <div>
+            <div className="w-full">{props.name}</div>
+            <ul>
+                {props.items.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default SkillCard;
