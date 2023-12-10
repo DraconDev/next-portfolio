f1ca73f423dfb8198970e789f052be60ab515566
DraconDev <dracsharp@gmail.com>
2023-12-10 19:12:58 +0000

    add workcard desc

--- components/Work/WorkCard.tsx
+++ components/Work/WorkCard.tsx
@@ -7,7 +7,22 @@ type Props = {
 };
 
 const WorkCard = (props: Props) => {
-    return <div>WorkCard</div>;
+    return (
+        <div className="w-full p-2 flex flex-col">
+            <h1 className="text-2xl">{props.project.name}</h1>
+            {/* <Image /> */}
+            <div className="text-xl gap-1 flex justify-center">
+                {props.project.description.split(" ").map((item, index) => (
+                    <div
+                        key={index}
+                        className="bg-secondary-light rounded-md p-2 text-ac"
+                    >
+                        {item}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default WorkCard;
