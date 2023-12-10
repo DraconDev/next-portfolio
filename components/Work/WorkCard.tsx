e7269daa59920bcd3f68fe07feb474f2cea0b432
DraconDev <dracsharp@gmail.com>
2023-12-10 19:32:09 +0000

    add work pictures

--- components/Work/WorkCard.tsx
+++ components/Work/WorkCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Project } from "@/state/Context";
+import Image from "next/image";
 
 type Props = {
     project: Project;
@@ -10,7 +11,14 @@ const WorkCard = (props: Props) => {
     return (
         <div className="w-full p-2 flex flex-col">
             <h1 className="text-2xl">{props.project.name}</h1>
-            {/* <Image /> */}
+            <Image
+                src={`${props.project.image_path}`}
+                alt={`${props.project.name}`}
+                width={1200}
+                height={800}
+                className="object-cover "
+            />
+
             <div className="text-xl gap-1 flex justify-center">
                 {props.project.description.split(" ").map((item, index) => (
                     <div
